202fd49cb274bcd3d71c73cf430b214f75ccd42f
JuanDAW37 <jymtr1968@gmail.com>
2024-10-01 09:22:49 +0200

    Exercicio 18 listo

--- src/main/java/com/campusdual/classroom/Exercise18.java
+++ src/main/java/com/campusdual/classroom/Exercise18.java
@@ -1,8 +1,34 @@
 package com.campusdual.classroom;
 
+import com.campusdual.util.Utils;
+
+import java.io.ByteArrayOutputStream;
+
 public class Exercise18 {
 
 	public static void main(String[] args) {
+		int elem = Utils.integer("Introduce el número de elementos que debe de tener el array: ");
+		int[] array = createAndInitializeArray(elem);
+		showInlineArray(array);
+	}
 
+	public static int[] createAndInitializeArray(int elem){
+		int[] array = new int[elem];
+		for (int i = 0; i < array.length; i++){
+			array[i] = i+1;
+		}
+		return array;
+	}
+
+	public static void showInlineArray(int[] array){
+		String linea="";
+		for (int i = 0; i < array.length; i++){
+			if (i == array.length - 1){
+				linea += array[i];
+			}else{
+				linea += array[i] + " ";
+			}
+		}
+		System.out.println(linea);
 	}
 }
